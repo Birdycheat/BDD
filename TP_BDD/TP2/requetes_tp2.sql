d1fa36dd5c618bbf3ac13f4557bc51d7c1c7f1cd
Birdycheat <43277802+Birdycheat@users.noreply.github.com>
2021-11-02 17:06:45 +0100

    new file:   Cours/Chap1-2-nup.pdf 	new file:   Cours/Chap3.pdf 	new file:   TD et Corrections/BDTD1.pdf 	new file:   TD et Corrections/BDTD2.pdf 	new file:   TD et Corrections/BDTD3.pdf 	new file:   TD et Corrections/BDTD5.pdf 	new file:   TD et Corrections/BDTD6.pdf 	new file:   TD et Corrections/BDTD7.pdf 	new file:   TD et Corrections/DOC310118-3.pdf 	new file:   TD et Corrections/TD1Ex1.pdf 	new file:   TD et Corrections/TD1Ex2.pdf 	new file:   TD et Corrections/TD1Ex3.pdf 	new file:   TD et Corrections/TD1Ex4.pdf 	new file:   TD et Corrections/TD1Ex5.pdf 	new file:   TD et Corrections/TD1Ex6.pdf 	new file:   TD et Corrections/TD1Ex8.pdf 	new file:   TD et Corrections/TD2-Ex.pdf 	new file:   TD et Corrections/TD5Ex1.pdf 	new file:   TD et Corrections/TD5Ex2.pdf 	new file:   TD et Corrections/TD6Ex1.pdf 	new file:   TP_BDD/TP1/tp_create_pc_corr.pdf 	modified:   TP_BDD/TP2/requetes_tp2.sql

--- TP_BDD/TP2/requetes_tp2.sql
+++ TP_BDD/TP2/requetes_tp2.sql
@@ -1,42 +1,39 @@
-select distinct eleve.nume 
-from eleve 
-join 
-(
-    select nbfautes, nume 
-    from assiste 
-    where nbfautes <= 1
-) t2
-on (eleve.nume = t2.nume);
+-- Question 1
+select distinct nume 
+from assiste 
+where nbfautes <= 1;
 
-
-select eleve.nom 
+-- Question 2
+select distinct eleve.nom 
 from 
 (
     eleve 
     join 
     (
-        select distinct assiste.nume from 
-        assiste 
-        where datea like '%06%'
+        select distinct assiste.nume 
+        from assiste 
+        where datea like '%06-2018'
     ) t2 
     on (eleve.nume = t2.nume)
 );
 
-
+-- Question 3
 select distinct nume 
 from inscrit 
 where nbfautes >= 5;
 
-
+-- Question 4 
+-- Il faut faire attention ici : il existe peut-être des questions répertoriées 
+-- qui n'apparaissent pas dans les CDs de l'auto-école
 select count(*) 
 from 
 (
-    select numq 
+    select numq
     from question 
-    where diffic > 3
+    where diffic > 3 and numq in appartient
 );
 
-
+-- Question 5
 select count(*) 
 from 
 (
@@ -50,7 +47,7 @@ from
     )
 );
 
-
+-- Question 6
 select distinct nums, numcd from 
 serie minus
 select distinct appartient.nums, appartient.numc from 
@@ -62,6 +59,7 @@ where appartient.numq in
     where diffic > 3
 );
 
+-- Question 7
 select distinct appartient.nums, appartient.numc 
 from appartient 
 where appartient.numq in 
@@ -80,6 +78,7 @@ where appartient.numq in
     where diffic = 1
 );
 
+-- Question 8
 select numq 
 from question 
 where numq not in 
@@ -88,9 +87,11 @@ where numq not in
     from appartient
 );
 
+-- Question 9
 select max(nbfautes) 
 from inscrit;
 
+-- Question 10
 select nom, prenom 
 from eleve 
 join 
@@ -103,12 +104,14 @@ join
         from inscrit
     )
 ) p2
-on (eleve.nume = p2.nume) ;
+on (eleve.nume = p2.nume);
 
+-- Question 11
 select nume, max(nbfautes) 
 from assiste 
 group by nume;
 
+-- Question 12
 select datee, avg(nbfautes) 
 from inscrit   
 where datee = 
@@ -120,6 +123,7 @@ where datee =
 )
 group by datee;
 
+-- Question 13
 select distinct nume 
 from inscrit 
 where nbfautes > 5 and nume in 
@@ -136,6 +140,7 @@ where nbfautes > 5 and nume in
         la question ne précisant pas quel examen de code regarder, nous avons ici supposé
         que nous stockons une seule inscription à l'examen pour chaque élève (la plus récente)*/
 
+-- Question 14
 select nume, count(*)
 from
 (
@@ -161,7 +166,6 @@ select nume from
 where nombre > 4;
 
 -- Question 16
-
 select nume, max(nbfautes)
 from assiste
 group by nume
@@ -189,7 +193,6 @@ from
 join eleve
 on eleve.nume = assidus.nume;
 
-
 -- Question 18
 select nume 
 from
@@ -204,7 +207,6 @@ from
     group by nume
 )
 where nombre > 3; 
-
 
 -- Question 19
 select numq from
@@ -270,8 +272,15 @@ select datee, heuree
 from examen
 where not exists
 (
-    select nbfautes 
-    from inscrit
+    -- Sélection des examens qui ont au moins un admis 
+    select datee, heuree, nbfautes
+    from
+    (
+        select examen.datee, examen.heuree, inscrit.nbfautes--examen.datee, examen.heuree
+        from examen
+        full join inscrit
+        on examen.datee = inscrit.datee and examen.heuree = inscrit.heuree
+    )
     where nbfautes <= 5
 );
 
